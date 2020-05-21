b847a9dd44e43a848cb9fa82674fe07daed1f853
panhuaqing <panhuaqing@bluetop.com.cn>
2020-05-21 16:01:20 +0800

    修改注释和删掉多余类

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# aurora-base
+(1) 目录结构说明
+        aurora-base:
+           父版本 主要配置子模块需要的mmaven 及子模块相关信息
+
+        aurora-common：
+           公共模块主要负责封装相关公共的实体 工具类 等相关公共使用的类
+
+        aurora-config：
+            公共配置模块 主要所有公共相关的配置 拦截器 自定义 aop config配置
+
+        aurora-model：
+            实体模块：所有实体对象所在模块
+
+        aurora-service：
+            业务接口模块 业务接口 和实现类 及dao接口,mapper.xml
+
+        aurora-web:
+             对外访问的视图层，所有的controller，主要与页面交互
+
+(2)依赖关系：
+
+ 最基础的是模块：model,common,web 都直接依赖或者间接依赖
+   aurora-common，aurora-config
+ 依赖关系：
+  aurora-base -> aurora-model -> aurora-service -> aurora-web
+
+
+
+(3) 数据库文件脚本：
+   根目录下：mysql-aurora.sql 文件
+
+
--- aurora-web/src/main/java/com/aurora/web/generator/AutoFile.java
+++ aurora-web/src/main/java/com/aurora/web/generator/AutoFile.java
@@ -13,16 +13,16 @@ import java.util.List;
 public class AutoFile {
 
         /**
-         * 测试 run 执行 注意：不生成service接口 注意：不生成service接口 注意：不生成service接口
+         * 测试 run 执行
          * <p>
          * 配置方法查看 ConfigGenerator
          * </p>
          */
         public static void main(String[] args) {
             AutoGenerator mpg = new AutoGenerator();
-// 全局配置
+            // 全局配置
             GlobalConfig gc = new GlobalConfig();
-            gc.setOutputDir("H://");//代码生成存放位置D:\aurora-base\aurora-model\src\main\java\com\aurora\model
+            gc.setOutputDir("D://");//代码生成存放位置D:\aurora-base\aurora-model\src\main\java\com\aurora\model
             gc.setFileOverride(true);
             gc.setActiveRecord(false);
             gc.setEnableCache(false);// XML 二级缓存
@@ -30,68 +30,65 @@ public class AutoFile {
             gc.setBaseColumnList(false);// XML columList
             gc.setOpen(true);
             gc.setAuthor("PHQ");//此处填写代码生成人姓名
-// 自定义文件命名，注意 %s 会自动填充表实体属性！
-// gc.setMapperName("%sDao");
-// gc.setXmlName("%sDao");
+            // 自定义文件命名，注意 %s 会自动填充表实体属性！
+            // gc.setMapperName("%sDao");
+            // gc.setXmlName("%sDao");
             gc.setServiceImplName("%sService");
-// gc.setServiceImplName("%sServiceDiy");
-// gc.setControllerName("%sAction");
+            // gc.setServiceImplName("%sServiceDiy");
+            // gc.setControllerName("%sAction");
             mpg.setGlobalConfig(gc);
-// 数据源配置
+            // 数据源配置
             DataSourceConfig dsc = new DataSourceConfig();
             dsc.setDbType(DbType.MYSQL);
             dsc.setDriverName("com.mysql.jdbc.Driver");
             dsc.setUsername("root");//数据库用户名
             dsc.setPassword("");//数据库密码
             dsc.setUrl("jdbc:mysql://localhost:3306/aurora?characterEncoding=utf8&allowMultiQueries=true");//数据库连接地址
-/*dsc.setTypeConvert(new SqlServerTypeConvert(){
-jdbc:mysql://localhost:3306/aurora?useUnicode=true&characterEncoding=utf-8
-// 自定义数据库表字段类型转换【可选】
-@Override
-public DbColumnType processTypeConvert(String fieldType) {
-if (fieldType.contains("tinyint") ) {
-return DbColumnType.BLOB;
-}
-// 注意！！processTypeConvert 存在默认类型转换，
-//如果不是你要的效果请自定义返回、非如下直接返回。
-return super.processTypeConvert(fieldType);
-}
-});*/
+            /*dsc.setTypeConvert(new SqlServerTypeConvert(){
+            jdbc:mysql://localhost:3306/aurora?useUnicode=true&characterEncoding=utf-8
+            // 自定义数据库表字段类型转换【可选】
+            @Override
+            public DbColumnType processTypeConvert(String fieldType) {
+            if (fieldType.contains("tinyint") ) {
+            return DbColumnType.BLOB;
+            }
+            // 注意！！processTypeConvert 存在默认类型转换，
+            //如果不是你要的效果请自定义返回、非如下直接返回。
+            return super.processTypeConvert(fieldType);
+            }
+            });*/
             mpg.setDataSource(dsc);
-// 策略配置
+            // 策略配置
             StrategyConfig strategy = new StrategyConfig();
-// strategy.setTablePrefix("sys_");// 此处可以修改为您的表前缀
+            // strategy.setTablePrefix("sys_");// 此处可以修改为您的表前缀
             strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
             //设置实体类使用lombok
             strategy.setEntityLombokModel(true);
 
-/**
- * 第一步
- * 代码生成需要改动这里，将待生成的表名替换到下面
- *
- * 2017.11.2
- */
+            /**
+             * 第一步
+             * 代码生成需要改动这里，将待生成的表名替换到下面
+             *
+             */
             strategy.setInclude(new String[] { "sys_role" }); // 需要生成的表
-/**
- * 第二步
- * 去掉表名前缀
- *
- * 2017.11.2
- */
+            /**
+             * 第二步
+             * 去掉表名前缀
+             */
             strategy.setTablePrefix(new String[] { "sys_" });// 此处可以修改为您的表前缀
-// strategy.setExclude(new String[]{"test"}); // 排除生成的表
-// 自定义实体父类
+            // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+            // 自定义实体父类
             strategy.setSuperEntityClass("com.aurora.model.PageModel");
-// 自定义实体，公共字段
+            // 自定义实体，公共字段
             strategy.setSuperEntityColumns(
                     new String[] {});
-// 自定义 mapper 父类
+            // 自定义 mapper 父类
             strategy.setSuperMapperClass("com.baomidou.mybatisplus.mapper.BaseMapper");
-// 自定义 service 父类
+            // 自定义 service 父类
             //strategy.setSuperServiceImplClass("com.mayi.test.base.BaseService");
-// 自定义 controller 父类
+            // 自定义 controller 父类
             //strategy.setSuperControllerClass("com.mayi.test.base.BaseController");
-          //逻辑删除属性名称
+             //逻辑删除属性名称
             strategy.setLogicDeleteFieldName("is_deleted");
             List<TableFill> tableFillList= CollUtil.newArrayList();
             TableFill fill=new TableFill("update_time", FieldFill.INSERT_UPDATE);
@@ -103,25 +100,23 @@ return super.processTypeConvert(fieldType);
             strategy.setTableFillList(tableFillList);
 
 
-// 自定义 service 实现类父类
-//strategy.setSuperServiceImplClass("org.ibase4j.core.base.BaseService");
-// 自定义 controller 父类
-//strategy.setSuperControllerClass("org.ibase4j.core.base.AbstractController");
-// 【实体】是否生成字段常量（默认 false）
-// public static final String ID = "test_id";
-// strategy.setEntityColumnConstant(true);
-// 【实体】是否为构建者模型（默认 false）
-// public User setName(String name) {this.name = name; return this;}
-// strategy.setEntityBuliderModel(true);
+            // 自定义 service 实现类父类
+            //strategy.setSuperServiceImplClass("org.ibase4j.core.base.BaseService");
+            // 自定义 controller 父类
+            //strategy.setSuperControllerClass("org.ibase4j.core.base.AbstractController");
+            // 【实体】是否生成字段常量（默认 false）
+            // public static final String ID = "test_id";
+            // strategy.setEntityColumnConstant(true);
+            // 【实体】是否为构建者模型（默认 false）
+            // public User setName(String name) {this.name = name; return this;}
+            // strategy.setEntityBuliderModel(true);
             mpg.setStrategy(strategy);
-// 包配置
+            // 包配置
             PackageConfig pc = new PackageConfig();
             pc.setParent("com.aurora");//此处填写包名
             /**
              * 第三步
              * 按模块调整代码放置目录
-             *
-             * 2017.11.2
              */
             pc.setEntity("sys.model");
             pc.setMapper("sys.dao");
@@ -130,18 +125,18 @@ return super.processTypeConvert(fieldType);
             pc.setService("sys.service");
             pc.setController("sys.web");
             mpg.setPackageInfo(pc);
-// 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
-/*InjectionConfig cfg = new InjectionConfig() {
-public void initMap() {
-Map<String, Object> map = new HashMap<String, Object>();
-map.put("providerClass", "ISysProvider");
-map.put("providerClassPackage", "org.ibase4j.provider.ISysProvider");
-this.setMap(map);
-}
-};
-mpg.setCfg(cfg);*/
-// 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/template 下面内容修改，
-// 放置自己项目的 src/main/resources/template 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+            // 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
+            /*InjectionConfig cfg = new InjectionConfig() {
+            public void initMap() {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("providerClass", "ISysProvider");
+            map.put("providerClassPackage", "org.ibase4j.provider.ISysProvider");
+            this.setMap(map);
+            }
+            };
+            mpg.setCfg(cfg);*/
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/template 下面内容修改，
+        // 放置自己项目的 src/main/resources/template 目录下, 默认名称一下可以不配置，也可以自定义模板名称
             TemplateConfig tc = new TemplateConfig();
             tc.setEntity("templates/entity.java.vm");
             tc.setMapper("templates/mapper.java.vm");
@@ -150,7 +145,7 @@ mpg.setCfg(cfg);*/
             tc.setService("templates/service.java.vm");
             tc.setController("templates/controller.java.vm");
             mpg.setTemplate(tc);
-// 执行生成
+            // 执行生成
             mpg.execute();
             System.out.println("代码生成完毕");
         }
